ae506f00bec183a7d62cfde06d9cbeaf11f2f7ad
Alex Fiestas <alex@eyeos.org>
2009-08-29 06:15:51 +0000

    Removed the dependency of avdevice, geting the code ready to add the vlc webcam widget.

--- src/webcamwidget.cpp
+++ src/webcamwidget.cpp
@@ -27,80 +27,80 @@
 #include <KTemporaryFile>
 #include <QDebug>
 #include <kio/copyjob.h>
-#include "webcamretriever.h"
+// #include "webcamretriever.h"
 
 WebcamWidget::WebcamWidget(QWidget* parent,WebcamRetriever *videoRetriever) : QLabel(parent)
 {
-	setRetriever(videoRetriever);
-	qDebug("%d", mRetriever->isAvailable());
+// 	setRetriever(videoRetriever);
+// 	qDebug("%d", mRetriever->isAvailable());
 }
 
 WebcamWidget::~WebcamWidget()
 {
-	mRetriever->markDone();
+// 	mRetriever->markDone();
 }
 
 void WebcamWidget::setRetriever(WebcamRetriever *videoRetriever)
 {
-	this->setMinimumSize(200,200);
-	setScaledContents(false);
-	mRetriever = videoRetriever;
-	connect(mRetriever, SIGNAL(imageReady()), SLOT(slotUpdateImage()));
-	connect(mRetriever, SIGNAL(finished()), SLOT(mRetreiverFinished()));
-	connect(mRetriever, SIGNAL(videoDeviceError()), SLOT(mRetreiverFinished()));
+// 	this->setMinimumSize(200,200);
+// 	setScaledContents(false);
+// 	mRetriever = videoRetriever;
+// 	connect(mRetriever, SIGNAL(imageReady()), SLOT(slotUpdateImage()));
+// 	connect(mRetriever, SIGNAL(finished()), SLOT(mRetreiverFinished()));
+// 	connect(mRetriever, SIGNAL(videoDeviceError()), SLOT(mRetreiverFinished()));
 }
 
 bool WebcamWidget::takePhoto(const KUrl& dest)
 {
-	if(!mRetriever->isAvailable())
-		return false;
-	
-	QString path;
-	if(dest.isLocalFile())
-		path=dest.toLocalFile();
-	else
-		path=KStandardDirs::locateLocal("appdata", "last.png");
-	
-	mRetriever->lock().lockForRead();
-	bool ret = mRetriever->image().save(path);
-	mRetriever->lock().unlock();
-	
-	if(ret) {
-		if(dest.isLocalFile())
-			emit photoTaken(dest);
-		else {
-			KIO::CopyJob* job=KIO::move(KUrl(path), dest);
-			job->setAutoDelete(true);
-			connect(job, SIGNAL(copyingDone(KIO::Job*,KUrl,KUrl,time_t,bool,bool)),
-					this, SLOT(emitKIOPhotoTaken(KIO::Job*,KUrl,KUrl,time_t,bool,bool)));
-			job->start();
-		}
-	}
-	
-	return ret;
+// 	if(!mRetriever->isAvailable())
+// 		return false;
+// 	
+// 	QString path;
+// 	if(dest.isLocalFile())
+// 		path=dest.toLocalFile();
+// 	else
+// 		path=KStandardDirs::locateLocal("appdata", "last.png");
+// 	
+// 	mRetriever->lock().lockForRead();
+// 	bool ret = mRetriever->image().save(path);
+// 	mRetriever->lock().unlock();
+// 	
+// 	if(ret) {
+// 		if(dest.isLocalFile())
+// 			emit photoTaken(dest);
+// 		else {
+// 			KIO::CopyJob* job=KIO::move(KUrl(path), dest);
+// 			job->setAutoDelete(true);
+// 			connect(job, SIGNAL(copyingDone(KIO::Job*,KUrl,KUrl,time_t,bool,bool)),
+// 					this, SLOT(emitKIOPhotoTaken(KIO::Job*,KUrl,KUrl,time_t,bool,bool)));
+// 			job->start();
+// 		}
+// 	}
+// 	
+// 	return ret;
 }
 
 void WebcamWidget::slotUpdateImage()
 {
 // 	qDebug("pam");
-	mRetriever->lock().lockForRead();
-	setPixmap(QPixmap::fromImage(mRetriever->image()));
-	mRetriever->lock().unlock();
+// 	mRetriever->lock().lockForRead();
+// 	setPixmap(QPixmap::fromImage(mRetriever->image()));
+// 	mRetriever->lock().unlock();
 }
 
 void WebcamWidget::mRetreiverFinished()
 {
-	qDebug() << "Restarting webcamWidget";
-	setAlignment(Qt::AlignCenter);
-	setPixmap(KIcon("camera-web").pixmap(128,128));
+// 	qDebug() << "Restarting webcamWidget";
+// 	setAlignment(Qt::AlignCenter);
+// 	setPixmap(KIcon("camera-web").pixmap(128,128));
 }
 
 QSize WebcamWidget::sizeHint() const
 {
-	return mRetriever->imageSize();
+// 	return mRetriever->imageSize();
 }
 
 void WebcamWidget::emitKIOPhotoTaken(KIO::Job* job, const KUrl& from, const KUrl & to, time_t t, bool , bool )
 {
-	emit photoTaken(to);
+// 	emit photoTaken(to);
 }
