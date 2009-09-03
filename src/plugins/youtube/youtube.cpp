43f1b233875b92c9ce11f446466c34bd31ae472a
Alex Fiestas <alex@eyeos.org>
2009-09-03 17:46:17 +0000

    First commit of youtube plugin, right now a execute plugin clone

---
+++ src/plugins/youtube/youtube.cpp
@@ -0,0 +1,65 @@
+/*************************************************************************************
+ *  Copyright (C) 2008-2009 by Aleix Pol <aleixpol@gmail.com>                        *
+ *  Copyright (C) 2008-2009 by Alex Fiestas <alex@eyeos.org>                         *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include "youtube.h"
+#include <KPluginFactory>
+#include <KAboutData>
+#include <KIcon>
+#include <KUrl>
+#include <QAction>
+#include <QDesktopServices>
+#include <KMessageBox>
+#include <KMimeType>
+
+K_PLUGIN_FACTORY(KamosoExecuteFactory, registerPlugin<ExecutePlugin>(); )
+K_EXPORT_PLUGIN(KamosoExecuteFactory(KAboutData("Youtube", "youtube",
+		ki18n("Youtube"), "0.1", ki18n("Upload videos directly to youtube"),
+		KAboutData::License_GPL)))
+
+ExecutePlugin::ExecutePlugin(QObject* parent, const QVariantList& args)
+	: KamosoPlugin(parent, args)
+{}
+
+QAction* ExecutePlugin::thumbnailsAction(const QList<KUrl>& urls)
+{
+	QAction* act=0;
+	mSelectedUrls.clear();
+	foreach(const KUrl& url, urls)
+	{
+		KMimeType::Ptr mime = KMimeType::findByUrl(url);
+		if(mime->name().startsWith("video/")) {
+			if(!act) {
+				act=new QAction(KIcon("system-run"), i18n("Upload to youtube"), 0);
+				connect(act, SIGNAL(triggered(bool)), SLOT(upload(bool)));
+			}
+		}
+		mSelectedUrls.append(url);
+	}
+	return act;
+}
+
+void ExecutePlugin::upload(bool)
+{
+	foreach(const KUrl& path, mSelectedUrls) {
+		bool corr=QDesktopServices::openUrl(path);
+		
+		if(!corr)
+			KMessageBox::error(0, i18n("Could not open %1", path.prettyUrl()));
+	}
+}
