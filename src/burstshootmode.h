699899b0aab22e838a4b6f71e274649180360b5e
Alex Fiestas <alex@eyeos.org>
2009-09-05 05:22:35 +0000

    First commit of burst mode (copy of photo one)

---
+++ src/burstshootmode.h
@@ -0,0 +1,37 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef BURSTSHOOTMODE_H
+#define BURSTSHOOTMODE_H
+
+#include "shootmode.h"
+
+
+class BurstShootMode : public ShootMode
+{
+	public:
+		BurstShootMode(Kamoso* camera);
+		
+// 		virtual QList<QAction*> actions();
+		virtual QWidget* mainAction();
+		virtual QIcon icon() const;
+		virtual QString name() const;
+};
+
+#endif // BURSTSHOOTMODE_H
