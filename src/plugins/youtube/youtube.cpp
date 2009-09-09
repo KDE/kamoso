eca27c98e773e9a995e5ba05b7aea3f4c3ba6c11
Alex Fiestas <alex@eyeos.org>
2009-09-09 14:37:39 +0000

    Removed unused buttom, it have no sense in this dialog

--- src/plugins/youtube/youtube.cpp
+++ src/plugins/youtube/youtube.cpp
@@ -102,7 +102,7 @@ QMap<QString, QString> YoutubePlugin::showVideoDialog()
 	KDialog *dialog = new KDialog();
 	dialog->setMainWidget(videoWidget);
 	dialog->setCaption(i18n("Video information:"));
-	dialog->setButtons(KDialog::Ok | KDialog::Cancel);
+	dialog->setButtons(KDialog::Ok);
 	dialog->setMinimumWidth(425);
 	dialog->setMinimumHeight(315);
 	dialog->setMaximumWidth(425);
