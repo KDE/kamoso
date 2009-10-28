4138e6fd4ac07c5cba448024ea3ab3b791a444d6
Alex Fiestas <alex@eyeos.org>
2009-10-28 14:48:39 +0000

    Ups.......

--- build/src/plugins/trash/CMakeFiles/trash_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "trash_automoc.cpp"
-  "CMakeFiles/trash_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/trash_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/src/plugins/youtube/CMakeFiles/youtube_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "youtube_automoc.cpp"
-  "CMakeFiles/youtube_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/youtube_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
