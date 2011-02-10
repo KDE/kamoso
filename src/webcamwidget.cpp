e175f7c6502adf62f77ee9b3d371fe010ba4431f
Aleix Pol <aleixpol@kde.org>
2011-02-10 03:35:07 +0100

    Request the proper format, otherwise we get random black screens :)

--- src/webcamwidget.cpp
+++ src/webcamwidget.cpp
@@ -334,8 +334,8 @@ QByteArray WebcamWidget::basicPipe()
 
     //Accepted capabilities
     pipe +=
-    " ! video/x-raw-yuv, width=640, height=480, framerate=15/1;"
-       "video/x-raw-yuv, width=640, height=480, framerate=30/1;"
+    " ! video/x-raw-yuv, format=(fourcc)I420, width=640, height=480, framerate=15/1;"
+       "video/x-raw-yuv, format=(fourcc)I420, width=640, height=480, framerate=30/1;"
        "video/x-raw-rgb, width=640, height=480, framerate=15/1;"
        "video/x-raw-rgb, width=640, height=480, framerate=30/1"
 
