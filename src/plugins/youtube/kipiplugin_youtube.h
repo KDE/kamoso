15d1e050ad24ec71b2cf541f47caba389dfec0c7
Alex Fiestas <afiestas@kde.org>
2011-05-19 19:04:55 +0200

    Fix header adding the right licence and copyright.

--- src/plugins/youtube/kipiplugin_youtube.h
+++ src/plugins/youtube/kipiplugin_youtube.h
@@ -1,6 +1,6 @@
 /*************************************************************************************
- *  Copyright (C) 2008-2009 by Aleix Pol <aleixpol@kde.org>                          *
- *  Copyright (C) 2008-2009 by Alex Fiestas <alex@eyeos.org>                         *
+ *  Copyright (C) 2008-2011 by Aleix Pol <aleixpol@kde.org>                          *
+ *  Copyright (C) 2008-2011 by Alex Fiestas <afiestas@kde.org>                       *
  *                                                                                   *
  *  This program is free software; you can redistribute it and/or                    *
  *  modify it under the terms of the GNU General Public License                      *
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU General Public License                *
  *  along with this program; if not, write to the Free Software                      *
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
- *************************************************************************************/
+ *************************************************************************************//
 
 #include <KPasswordDialog>
 #include <KUrl>
--- src/plugins/youtube/youtubejob.cpp
+++ src/plugins/youtube/youtubejob.cpp
@@ -1,6 +1,6 @@
 /*************************************************************************************
- *  Copyright (C) 2008-2010 by Aleix Pol <aleixpol@gmail.com>                        *
- *  Copyright (C) 2008-2010 by Alex Fiestas <alex@eyeos.org>                         *
+ *  Copyright (C) 2008-2011 by Aleix Pol <aleixpol@kde.org>                          *
+ *  Copyright (C) 2008-2011 by Alex Fiestas <afiestas@kde.org>                       *
  *                                                                                   *
  *  This program is free software; you can redistribute it and/or                    *
  *  modify it under the terms of the GNU General Public License                      *
