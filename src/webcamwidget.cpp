2476318151ca0c133bc47f2f8b420bf032e5595b
Alex Fiestas <afiestas@kde.org>
2011-05-22 13:39:04 +0200

    Added more capabilities, we have to add some ehuristic here... :/

--- src/webcamwidget.cpp
+++ src/webcamwidget.cpp
@@ -350,7 +350,9 @@ QByteArray WebcamWidget::basicPipe()
     pipe +=
     " ! ffmpegcolorspace"
     " ! video/x-raw-yuv, format=(fourcc)I420, width=640, height=480, framerate=15/1;"
-       "video/x-raw-yuv, format=(fourcc)I420, width=640, height=480, framerate=30/1"
+    " video/x-raw-yuv, format=(fourcc)I420, width=640, height=480, framerate=24/1;"
+    " video/x-raw-yuv, format=(fourcc)I420, width=640, height=480, framerate=30/1;"
+    " video/x-raw-yuv, format=(fourcc)I420, width=352, height=288, framerate=15/1"
 
     //Basic plug-in for video controls
     " ! gamma name=gamma"
