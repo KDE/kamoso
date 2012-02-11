cfc7da273a1c0b37daee9370255e3e30d0dfd22a
Aleix Pol <aleixpol@kde.org>
2012-02-11 23:15:51 +0100

    show some thumbnails on the images view

--- src/kamosodirmodel.cpp
+++ src/kamosodirmodel.cpp
@@ -22,7 +22,11 @@
 
 KamosoDirModel::KamosoDirModel(QObject* parent)
     : KDirModel(parent)
-{}
+{
+    QHash<int, QByteArray> roles = roleNames();
+    roles.insert(Path, "path");
+    setRoleNames(roles);
+}
 
 void KamosoDirModel::setUrl(const QUrl& url)
 {
@@ -42,4 +46,12 @@ void KamosoDirModel::setMimeFilter(const QStringList& mimes)
 QStringList KamosoDirModel::mimeFilter() const
 {
     return dirLister()->mimeFilters();
+}
+
+QVariant KamosoDirModel::data(const QModelIndex& index, int role) const
+{
+    if(role == Path) {
+        return QUrl(qvariant_cast<KFileItem>(index.data(KDirModel::FileItemRole)).url());
+    } else
+        return KDirModel::data(index, role);
 }
