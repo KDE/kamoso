37060d454720d2a67d9d6e5623beb292b7f580fc
Rex Dieter <rdieter@math.unl.edu>
2012-12-21 13:38:39 -0600

    make KAboutData version match reality

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@
 
 int main(int argc, char *argv[])
 {
-    KAboutData about("kamoso", 0, ki18n(("Kamoso")), "1.90", ki18n("Webcam picture retriever"),
+    KAboutData about("kamoso", 0, ki18n(("Kamoso")), "2.0.2", ki18n("Webcam picture retriever"),
                 KAboutData::License_GPL, ki18n("(C) 2008-2010 Alex Fiestas and Aleix Pol"));
     about.addAuthor( ki18n("Aleix Pol Gonzalez"), ki18n("Semaphore hacker"), "aleixpol@kde.org" );
     about.addAuthor( ki18n("Alex Fiestas"), ki18n("Coffee drinker"), "alex@eyeos.org" );
