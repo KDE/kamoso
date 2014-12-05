9582145cd3db1a4a901c7193217254d9042149c1
Aleix Pol <aleixpol@kde.org>
2014-12-05 19:08:43 +0100

    Move plugins in a separate directory

--- src/share/plugins/dummy/dummyplugin.cpp
+++ src/share/plugins/dummy/dummyplugin.cpp
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
  ************************************************************************************/
 
-#include "../shareinterface.h"
+#include <shareinterface.h>
 #include <QDebug>
 #include <QTimer>
 #include <QStandardPaths>
