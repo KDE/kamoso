b5895e6edfbd3aed4b0c6a80bb3d42e10de1e9ce
Albert Astals Cid <aacid@kde.org>
2014-12-20 16:06:12 +0100

    Fix search
    CCMAIL: aleixpol@kde.org

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT rc.cpp src/*.cpp src/plugins/*/*.cpp  -o $podir/kamoso.pot
+$XGETTEXT `find . -name \*.cpp`  -o $podir/kamoso.pot
 
