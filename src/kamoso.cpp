5b11cb35ed29770b7956dcb35376d2d81a47c1d7
Taegil Bae <esrevinu@gmail.com>
2016-07-31 05:00:53 +0900

    Fix date of the filename

--- src/kamoso.cpp
+++ src/kamoso.cpp
@@ -50,7 +50,7 @@ Kamoso::~Kamoso()
 
 QUrl Kamoso::fileNameSuggestion(const QUrl &saveUrl, const QString &name, const QString& extension) const
 {
-    const QString date = QDateTime::currentDateTime().toString(QStringLiteral("yyyy-mm-dd_hh-mm-ss"));
+    const QString date = QDateTime::currentDateTime().toString(QStringLiteral("yyyy-MM-dd_hh-mm-ss"));
     const QString initialName =  QStringLiteral("%1_%2.%3").arg(name, date, extension);
 
     QUrl path(saveUrl.toString() + '/' + initialName);
